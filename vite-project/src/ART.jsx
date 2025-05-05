41f1c70545145c337d37f90c40be2f0de2a30a51
tanaramsuthar <tanaramsuthar01@gmail.com>
2025-05-05 12:16:39 +0530

    first vite project

---
+++ vite-project/src/ART.jsx
@@ -0,0 +1,13 @@
+function ART()
+{
+    return (
+        <>
+        <h2>Hey I'm </h2>
+        <h3>Tanaram suthar.</h3>
+        <h3>I'm a full stack developer.</h3>
+        </>
+       
+      )
+}
+
+export default ART
