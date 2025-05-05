e3a97c02ef7443e2b1d4ae4d310ce64704beb352
tanaramsuthar <tanaramsuthar01@gmail.com>
2025-05-05 17:55:37 +0530

    add some more function

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ART from './ART.jsx/'
 
+
+function myApp(){
+  return(
+    <> 
+    <h1>hello world !</h1>
+    </>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a', 
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google'
+)
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ART/>
-  </StrictMode>,
+  // <StrictMode>
+    // myApp()  // function
+       <ART/>
+      // reactElement // create using/by React 
+      // anotherElement // object 
 )
